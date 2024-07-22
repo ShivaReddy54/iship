a5a275c357c9768d68202c6e21216e603c3efb62
Shiva Reddy <shivareddy00005@gmail.com>
2024-07-22 10:29:10 +0530

    added appbar to screen2

--- lib/Screens/screen2.dart
+++ lib/Screens/screen2.dart
@@ -5,6 +5,39 @@ class Screen2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "Level - 01",
+            style: TextStyle(
+                color: Color.fromRGBO(13, 131, 70, 1),
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+        actions: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 4,
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.filter_alt,
+                    size: 25,
+                  ))
+            ],
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView(
+          children: [],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:iship/Screens/screen1.dart';
+import 'package:iship/Screens/screen2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: ' Iship ',
-      home: Screen1(),
+      debugShowCheckedModeBanner: false,
+      home: Screen2(),
       routes: {},
     );
   }
